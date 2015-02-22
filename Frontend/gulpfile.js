153a38898ba39966ed9bddad0c583215efd93a8b
Arjen Smit <arjen@alnitak.nl>
2015-02-22 17:20:28 +0100

    Added a basic sass implementation

--- Frontend/gulpfile.js
+++ Frontend/gulpfile.js
@@ -9,25 +9,42 @@ var     gulp = require('gulp'),
         concat = require('gulp-concat'),
         sourcemaps = require ('gulp-sourcemaps'),
         gulpif = require('gulp-if'),
-        livereload = require('gulp-livereload');
+        livereload = require('gulp-livereload'),
+        plumber = require('gulp-plumber')
+        sass = require('gulp-sass'),
+        csso = require('gulp-csso');
 
 /* Executable tasks */
 gulp.task('default', function() {
     gulp.start('head-js');
     gulp.start('body-js');
+    gulp.start('sass');
 });
 
 gulp.task('dev', function() {
     productionDefualtEnviorment = false;
-    gulp.start('head-js');
-    gulp.start('body-js');
+    gulp.start('default');
 });
 
 gulp.task('watch', function() {
     productionDefualtEnviorment = false;
     livereload.listen();
     gulp.watch(['./javascript/*.js', './bower_components/*.js'], ['head-js','body-js']);
-    
+    gulp.watch(['./scss/**/*.scss', './bower_components/**/*.scss'],['sass']);
+});
+
+/* Stylesheet compilation */
+gulp.task('sass', function () {
+    gulp.src('scss/*.scss')
+        .pipe(plumber())
+        .pipe(sourcemaps.init())
+        .pipe(sass(
+            { includePaths : ['scss/'] 
+            }
+        ))
+        .pipe(gulpif(productionDefualtEnviorment, csso(), sourcemaps.write('../maps')))
+        .pipe(gulp.dest(assetsDirectory + 'css/'))
+        .pipe(livereload());
 });
 
 /* Javascript generation for head.min.js */
@@ -40,7 +57,6 @@ gulp.task('head-js', function() {
     .pipe(gulpif(productionDefualtEnviorment, uglify(), sourcemaps.write('../maps')))
     .pipe(gulp.dest(assetsDirectory + 'javascript/'))
     .pipe(livereload());
-    
 });
 
 /* Javascript generation for body.min.js */
