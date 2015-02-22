13fbd732728880912aebc3bf839b83bd68bcebb2
Arjen Smit <arjen@alnitak.nl>
2015-02-22 16:54:44 +0100

    Added all things javascript related to the mix

--- Frontend/gulpfile.js
+++ Frontend/gulpfile.js
@@ -1,5 +1,57 @@
-var gulp = require('gulp');
+/* Variables: */
+var     documentRoot = "../web/";
+var     assetsDirectory = documentRoot + "assets/";
+var     productionDefualtEnviorment = true;
 
+/* Gulp Requirements */
+var     gulp = require('gulp'),
+        uglify = require('gulp-uglify'),
+        concat = require('gulp-concat'),
+        sourcemaps = require ('gulp-sourcemaps'),
+        gulpif = require('gulp-if'),
+        livereload = require('gulp-livereload');
+
+/* Executable tasks */
 gulp.task('default', function() {
-  // place code for your default task here
+    gulp.start('head-js');
+    gulp.start('body-js');
+});
+
+gulp.task('dev', function() {
+    productionDefualtEnviorment = false;
+    gulp.start('head-js');
+    gulp.start('body-js');
+});
+
+gulp.task('watch', function() {
+    productionDefualtEnviorment = false;
+    livereload.listen();
+    gulp.watch(['./javascript/*.js', './bower_components/*.js'], ['head-js','body-js']);
+    
+});
+
+/* Javascript generation for head.min.js */
+gulp.task('head-js', function() {
+    gulp.src([
+        "javascript/head.js"
+    ])
+    .pipe(sourcemaps.init())
+    .pipe(concat('head.js'))
+    .pipe(gulpif(productionDefualtEnviorment, uglify(), sourcemaps.write('../maps')))
+    .pipe(gulp.dest(assetsDirectory + 'javascript/'))
+    .pipe(livereload());
+    
+});
+
+/* Javascript generation for body.min.js */
+gulp.task('body-js', function() {
+    gulp.src([
+        "bower_components/jquery/dist/jquery.js",
+        "javascript/body.js"
+    ])
+    .pipe(sourcemaps.init())
+    .pipe(concat('body.js'))
+    .pipe(gulpif(productionDefualtEnviorment, uglify(), sourcemaps.write('../maps')))
+    .pipe(gulp.dest(assetsDirectory + 'javascript/'))
+    .pipe(livereload());;
 });
